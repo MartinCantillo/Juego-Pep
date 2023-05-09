aa4fe4cfc8236b508ba7fda8c88096f48090a39c
Jairo Berdugo <jairoberdugoh@gmail.com>
2023-05-09 09:42:07 -0500

    agg model-pregunta

---
+++ model/pregunta.py
@@ -0,0 +1,26 @@
+from config.db import bd, app, ma
+
+class Users(bd.Model):
+    __tablename__ ='tbl_pregunta'
+
+    id = bd.Column(bd.Integer, primary_key = True)
+    NomCorto=bd.Column(bd.String(50))
+    Idtematica_FK = bd.Column(bd.Integer,bd.ForeignKey('tbl_tematica.id'))
+    enunciado=bd.Column(bd.String(100))
+    puntos=bd.Column(bd.Integer)
+    IdNiveles_Fk=bd.Column(bd.Integer)
+
+    def __init__(self, id, NomCorto,Idtematica_FK,enunciado,puntos,IdNiveles_Fk):
+        self.id = id
+        self.NomCorto = NomCorto
+        self.Idtematica_FK =Idtematica_FK     
+        self.enunciado= enunciado
+        self.puntos=puntos
+        self.IdNiveles_Fkk=IdNiveles_Fk
+    
+with app.app_context():
+    bd.create_all()
+    
+class UsersSchema(ma.Schema):
+    class Meta:
+        fields = ('id','NomCorto','Idtematica_FK','enunciado','puntos','IdNiveles_Fk')
