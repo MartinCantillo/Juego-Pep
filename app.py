879a3a5608faa6a7ed396cea3c7d536602f19e9e
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-05-11 15:53:02 -0500

    link a registro agregado

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def index():
     nombre= "Loggin"
     return render_template('Login.html')
 
+@app.route("/Registro", methods=['GET'])
+def registrar():
+    nombre= "registrar"
+    return  render_template('Registro.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9566)
