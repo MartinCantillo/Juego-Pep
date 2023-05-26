b9a69fea1be0c4347e6b5df01450a523016c8300
Andrey-Ruiz <ruizandrey90@gmail.com>
2023-05-26 15:48:33 -0500

    funcion de registro y cambio de contraseña

--- config/static/js/registro.js
+++ config/static/js/registro.js
@@ -7,19 +7,21 @@ function saveuser(){
 
 
     if (txtcontraf.value == txtcontra.value){
-    axios.post(endpoint,{
-        'nom_usuario' : txtnom.value ,
-        'IdCategoria_Fk' : 1,
-        'estado': 'a',
-        'clave_usuario': txtcontra.value,
-        'avatar_rut': '/static/img/avatar/avatar1.jpg',
-        'emailusuario_pk': txtemail.value
-    })
-    .then(function (response) {
-      alert(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
+        axios.post(endpoint,{
+            'nom_usuario' : txtnom.value ,
+            'IdCategoria_Fk' : 1,
+            'estado': 'a',
+            'clave_usuario': txtcontra.value,
+            'avatar_rut': '/static/img/avatar/avatar1.jpg',
+            'emailusuario_pk': txtemail.value
+        })
+        .then(function (response) {
+          alert("Usuario creado");
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }else{
+      alert("No coinciden las contraseñas")
+    }
 }
