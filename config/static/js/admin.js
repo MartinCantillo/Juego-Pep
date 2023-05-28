f10a138456d1391cf82f6df9170afaaf5ab1f86e
OmarHdzO <omare-hernandezo@unilibre.edu.co>
2023-05-28 15:22:02 -0500

    nuevo

--- config/static/js/admin.js
+++ config/static/js/admin.js
@@ -22,3 +22,29 @@ createTematicsCard.addEventListener("click", () => {
   window.location.href = "/creartematicas";
 });
 */
+
+
+var d = new Date(); var month = new Array(12); month[0] = 'Enero'; month[1] = 'Febrero'; month[2] = 'Marzo'; month[3] = 'Abril'; month[4] = 'Mayo'; month[5] = 'Junio'; month[6] = 'Julio'; month[7] = 'Agosto'; month[8] = 'Septiembre'; month[9] = 'Octubre'; month[10] = 'Noviembre'; month[11] = 'Diciembre'; var todaysDate = +d.getDate() + ' de ' + month[d.getUTCMonth()] + ' del ' + d.getUTCFullYear();
+document.getElementById('fechahoy').innerHTML = 'Hoy es: ' + todaysDate;
+
+function show() {
+  var Digital = new Date()
+  var hours = Digital.getHours()
+  var minutes = Digital.getMinutes()
+  var seconds = Digital.getSeconds()
+  var dn = "AM"
+  if (hours > 12) {
+    dn = "PM"
+    hours = hours - 12
+  }
+  if (hours == 0)
+    hours = 12
+  if (minutes <= 9)
+    minutes = "0" + minutes
+  if (seconds <= 9)
+    seconds = "0" + seconds
+  document.Tick.Clock.value = hours + ":" + minutes + ":" +
+    seconds + " " + dn
+  setTimeout("show()", 1000)
+}
+show();
